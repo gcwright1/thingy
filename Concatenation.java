8b32b9a312b3f1ebea361c0b44536bc2f8eceda0
gcwright1 <42742757+gcwright1@users.noreply.github.com>
2018-09-05 10:56:59 -0400

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,12 @@
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		int x=3, y=4;
+		System.out.println("My favorite number is " +x+y);
+		System.out.println("You are " + (x+y) + " years old");
+		System.out.println("I have " +x+y+ " eggplants");
+
+	}
+
+}
