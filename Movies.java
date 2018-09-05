644410ef04b5283fb6784662de0f2c39fe2783b6
gcwright1 <42742757+gcwright1@users.noreply.github.com>
2018-09-05 11:15:36 -0400

    Add files via upload

---
+++ Movies.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Movies {
+
+	public static void main(String[] args) {
+		//declare variables
+		Scanner greyson = new Scanner(System.in);
+		double adultTickets;
+		double childrenTickets;
+		double total;
+		
+		//ask questions & get answers
+		System.out.println ("How many adult tickets?");
+		adultTickets=10.25*greyson.nextDouble ();
+		System.out.println ("How many children tickets?");
+		childrenTickets=5*greyson.nextDouble ();
+		
+		//do math
+		total = adultTickets+childrenTickets;
+		
+		
+		//show the answer
+		System.out.println ("The total price will be $"+(1.06*(total)));
+		
+
+	}
+
+}
