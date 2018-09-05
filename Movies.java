0da6544952301b94d95d9cc887ede645ca5ddb48
gcwright1 <42742757+gcwright1@users.noreply.github.com>
2018-09-05 11:18:33 -0400

    repository practice

--- Movies.java
+++ Movies.java
@@ -7,20 +7,20 @@ public class Movies {
 		double adultTickets;
 		double childrenTickets;
 		double total;
-		
+
 		//ask questions & get answers
 		System.out.println ("How many adult tickets?");
 		adultTickets=10.25*greyson.nextDouble ();
 		System.out.println ("How many children tickets?");
 		childrenTickets=5*greyson.nextDouble ();
-		
+
 		//do math
 		total = adultTickets+childrenTickets;
-		
-		
+
+
 		//show the answer
 		System.out.println ("The total price will be $"+(1.06*(total)));
-		
+		blah
 
 	}
 
